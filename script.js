c5dc14bb16c8841f92744e06e52fa08fc60e94b9
HeriRandri <hrandri30@gmail.com>
2023-11-23 10:13:26 +0300

    calc1

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-const suppr = document.getElementById("mamafa");
 const tab = document.getElementById("tableau");
 const btn = document.getElementsByClassName("chiffre");
-
+const premierChiffre = document.getElementById("premierChiffre");
+const deuxiemeChiffre = document.getElementById("deuxiemeChiffre");
+var resultat = 0;
 function calcul(btnrehetra) {
   tab.value = tab.value + btnrehetra.innerText;
 }
@@ -12,10 +13,24 @@ for (let i = 0; i < btn.length; i++) {
   });
 }
 
+const suppr = document.getElementById("mamafa");
 suppr.addEventListener("click", function () {
   tab.value = "";
 });
 
+const egal = document.getElementById("egal");
+egal.addEventListener("click", function () {
+  tab.value = calculeo();
+});
+
 const plus = document.getElementById("plus");
-function add(chiffre) {}
-console.log(add(3, 5));
+if (
+  (plus.onclick = function () {
+    tab.value = "+";
+  })
+) {
+}
+
+function calculeo(a, d) {
+  return a + d;
+}
